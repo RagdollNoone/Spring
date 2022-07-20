c40ff2b7dadfbc8c2005188486b71db7773be680
zhoushanhong <zhoushanhong@cs.sh.cn>
2022-07-20 14:38:52 +0800

    测试通过

--- dubbo-filter/server/src/main/java/com/impl/DemoServiceImpl.java
+++ dubbo-filter/server/src/main/java/com/impl/DemoServiceImpl.java
@@ -1,7 +1,18 @@
 package com.impl;
 
+import com.IDemoService;
+import com.alibaba.fastjson.JSON;
+import com.request.DemoRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
 
+@Slf4j
 @DubboService
 public class DemoServiceImpl implements IDemoService {
+
+    @Override
+    public String invoke(DemoRequest request) {
+        log.info("[DemoServiceImpl][invoke]request: {}", JSON.toJSONString(request));
+        return "call DemoServiceImpl invoke success!";
+    }
 }
---
+++ dubbo-filter/server/src/test/java/com/CallTest.java
@@ -0,0 +1,28 @@
+package com;
+
+import com.request.DemoRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class CallTest {
+    @DubboReference
+    private IDemoService service;
+
+    @Test
+    public void callTest() {
+        DemoRequest request = new DemoRequest();
+        request.setSubject("test");
+
+        String result = service.invoke(request);
+        log.info("{}", result);
+    }
+
+
+}
