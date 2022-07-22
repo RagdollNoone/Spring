69365fab74cb01906d2bdab837f34758b22a6ddd
zhoushanhong <zhoushanhong@cs.sh.cn>
2022-07-22 16:18:36 +0800

    初步提交

---
+++ spring-demo/web/src/main/java/com/WebApplication.java
@@ -0,0 +1,11 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com"})
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class);
+    }
+}
