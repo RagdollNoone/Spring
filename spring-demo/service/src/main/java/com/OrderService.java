8cfa8c8b796cbdf355df373dd4fcbd0667f9b3f0
zhoushanhong <zhoushanhong@cs.sh.cn>
2022-07-28 16:55:25 +0800

    service层阶段提交

---
+++ spring-demo/service/src/main/java/com/OrderService.java
@@ -0,0 +1,18 @@
+package com;
+
+import com.model.OrderInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class OrderService {
+    public OrderInfo createOrder(Map<String, String> params) {
+//        Optional<> // TODO: 使用Optional和assert的配合
+
+        return null;
+    }
+}
