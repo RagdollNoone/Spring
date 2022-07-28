4ca502f4609ff65646d57f448f4e8a1027b7c783
zhoushanhong <zhoushanhong@cs.sh.cn>
2022-07-28 14:11:08 +0800

    test代码更新

--- spring-step-by-step/src/main/java/com/demo/DemoTest.java
+++ spring-step-by-step/src/main/java/com/demo/DemoTest.java
@@ -2,54 +2,40 @@ package com.demo;
 
 import com.demo.service.UserService;
 import com.demo.service.UserServiceInterface;
-import com.exception.ExceptionHandler;
 import com.spring.ApplicationContext;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class DemoTest {
+    private static final String USER_SERVICE_BEAN_NAME = "userService";
+
     public static void main(String[] args) {
         ApplicationContext context = new ApplicationContext(AppConfig.class);
 
         // 不同的测试案例
-//        test1(context);
-//        test2(context);
-//        test3(context);
-//        test4(context);
-        test5(context);
+        test1(context);
+        test2(context);
+        test3(context);
     }
 
     // 测试scope为singleton
-    private static void test1(ApplicationContext context) {
-        System.out.println(context.getBean("userService"));
-        System.out.println(context.getBean("userService"));
-        System.out.println(context.getBean("userService"));
-    }
-
     // 测试scope为prototype
-    private static void test2(ApplicationContext context) {
-        System.out.println(context.getBean("userService"));
-        System.out.println(context.getBean("userService"));
-        System.out.println(context.getBean("userService"));
+    private static void test1(ApplicationContext context) {
+        log.info("{}", context.getBean(USER_SERVICE_BEAN_NAME));
+        log.info("{}", context.getBean(USER_SERVICE_BEAN_NAME));
+        log.info("{}", context.getBean(USER_SERVICE_BEAN_NAME));
     }
 
     // 测试Autowired
-    private static void test3(ApplicationContext context) {
-        UserService userService = (UserService) context.getBean("userService");
+    private static void test2(ApplicationContext context) {
+        UserService userService = (UserService) context.getBean(USER_SERVICE_BEAN_NAME);
         userService.print();
         userService.printOrderService();
     }
 
-    // 测试assert
-    private static void test4(ApplicationContext context) {
-        try {
-            ApplicationContext localContext = new ApplicationContext(null);
-        } catch (Exception e) {
-            ExceptionHandler.handle(e);
-        }
-    }
-
     // 测试切面
-    private static void test5(ApplicationContext context) {
-        UserServiceInterface userService = (UserServiceInterface) context.getBean("userService");
+    private static void test3(ApplicationContext context) {
+        UserServiceInterface userService = (UserServiceInterface) context.getBean(USER_SERVICE_BEAN_NAME);
         userService.print();
         userService.printOrderService();
         userService.printAuthor();
