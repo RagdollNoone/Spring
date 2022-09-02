44a7014ede3374b68b713ff30781d0db0dbfaf7a
zhoushanhong <zhoushanhong@cs.sh.cn>
2022-09-02 11:57:05 +0800

    添加图灵项目

---
+++ tuling/spring-lesson-1/src/main/java/com/service/UserService.java
@@ -0,0 +1,12 @@
+package com.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class UserService {
+    public void test() {
+        System.out.println("[UserService][test]start");
+    }
+}
