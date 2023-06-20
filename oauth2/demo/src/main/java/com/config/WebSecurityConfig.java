797116eb8fc1958e60adfc109e857f60c3c46aeb
zhoushanhong <zhoushanhong@cs.sh.cn>
2023-06-20 10:13:44 +0800

    问题版本 需要引入新的pom依赖 redis相关的组件 由于不会递归查询依赖的版本 所以修改了parent页签

---
+++ oauth2/demo/src/main/java/com/config/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.config;
+
+import com.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // 获取用户信息
+        auth.userDetailsService(userService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().permitAll()
+                .and().authorizeRequests()
+                .antMatchers("/oauth/**").permitAll()
+//                .antMatchers("/order/**").permitAll()
+                .anyRequest().authenticated()
+                .and().logout().permitAll()
+                .and().csrf().disable();
+
+    }
+
+    // 密码模式引入
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
