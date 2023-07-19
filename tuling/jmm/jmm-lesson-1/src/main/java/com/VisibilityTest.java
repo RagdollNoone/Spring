e0ef07183526784408d7d33a1efb40775f328c40
zhoushanhong <zhoushanhong@cs.sh.cn>
2023-07-19 22:51:27 +0800

    添加笔记注释

--- tuling/jmm/jmm-lesson-1/src/main/java/com/VisibilityTest.java
+++ tuling/jmm/jmm-lesson-1/src/main/java/com/VisibilityTest.java
@@ -2,9 +2,22 @@ package com;
 
 import java.util.concurrent.locks.LockSupport;
 
+// 可见性测试案例
+// 线程B 控制 线程A 的执行
+
+// jmm内存模型 线程间共享内存模型
+// CPU -- 本地内存(缓存) -- 主内存
+
+// 内存屏障的理解
+// 缓存强制刷新
+// 底层是汇编lock前缀 保证当前缓存立刻刷回主存 并通知变量的其它副本失效(缓存过期)
 public class VisibilityTest {
-    private boolean flag = true; // volatile
-    private int count = 0; // Integer
+    // volatile在hotspot中的实现
+    // bytecodeInterpreter.cpp
+    private boolean flag = true; // 方法 1 volatile修饰 内存屏障
+    // 方法 6 volatile修饰
+    // 方法 7 Integer 最终count的值是final修饰的 final也会保证可见性
+    private int count = 0;
 
     public static void main(String[] args) throws Exception {
         VisibilityTest test = new VisibilityTest();
@@ -23,23 +36,28 @@ public class VisibilityTest {
         System.out.println(Thread.currentThread().getName() + " modify flag: " + flag);
     }
 
+    // 可见性的实现方式
+    // 内存屏障
+    // 上下文切换
     public void load() {
         System.out.println(Thread.currentThread().getName() + " start...");
 
         while (flag) {
             count++;
 
-            //1. jvm层面 storeLoad内存屏障    ===>  x86   lock替代了mfence
-            //2. 上下文切换   Thread.yield();
-
+            // 方法 2 内存屏障
 //            UnsafeFactory.getUnsafe().storeFence();
 
-//            Thread.yield();
+            // 方法 3 上下文切换(缓存过期) 从主存加载最新的值
+//            Thread.yield(); // 释放时间片
 
+            // 方法 4 依赖synchronized语义 底层是内存屏障
 //            System.out.println(count);
 
+            // 方法 5 内存屏障
 //            LockSupport.unpark(Thread.currentThread());
 
+            // 方法 6 内存屏障
 //            try {
 //                Thread.sleep(1);
 //            } catch (Exception e) {
